b8e0d3e0fe67c8a6eaea9d7db529ae0fb9f0a1bc
aldi <muha22170si@student.nurulfikri.ac.id>
2025-06-13 07:26:25 +0700

    add:geoexplorer-app

---
+++ websig/resources/views/livewire/geoexplorer.blade.php
@@ -0,0 +1,24 @@
+<div>
+    <!-- Floating Search Box -->
+    <div class="floating-search">
+        <h2>🌏 Geo Explorer</h2>
+        <div class="search-container">
+            <input type="text" id="locationInput" placeholder="Contoh: Jawa Barat, Tokyo, Brazil" />
+            <button onclick="searchLocation()">Cari</button>
+        </div>
+    </div>
+
+    <!-- Map container -->
+    <div id="map"></div>
+
+    <!-- Tombol toggle -->
+    <button id="toggleSidebarBtn" class="toggle-btn">☰</button>
+
+    <!-- Sidebar -->
+    <div id="sidebar" class="sidebar">
+        <div id="tabs" class="tabs"></div>
+        <div id="tabContents"></div>
+    </div>
+</div>
+
+</div>
--- websig/routes/web.php
+++ websig/routes/web.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Map as MapComponent;
+use App\Livewire\Geoexplorer as GeoexplorerComponent;
 
-use function PHPSTORM_META\map;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/map', MapComponent::class)->name('map');
+Route::get('/geoexplorer', GeoexplorerComponent::class)->name('geoexplorer');
